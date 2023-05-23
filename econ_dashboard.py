60cfe1b794e6b3be3970c02d6c710b653ebf063c
Tolga <dfavenfre@protonmail.com>
2023-05-23 16:04:00 +0300

    Update econ_dashboard.py

--- econ_dashboard.py
+++ econ_dashboard.py
@@ -1,7 +1,6 @@
 # packages
 import streamlit as st
 from datetime import datetime
-import yfinance as yf
 # Welcome to Econ Dashboard (Beta) 
 title_html = """
     <h1 style="color:black; font-size:36px;">Welcome to Econ Dashboard <span style="font-size:14px;">BETA</span></h1>
