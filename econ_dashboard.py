79aef224f29692c90b5bdd846b546c1581329cc1
Tolga <dfavenfre@protonmail.com>
2023-05-23 15:39:37 +0300

    Update econ_dashboard.py

--- econ_dashboard.py
+++ econ_dashboard.py
@@ -5,7 +5,6 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import tensorflow as tf
