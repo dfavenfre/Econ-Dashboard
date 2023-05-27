3fa041459b23bf1a36683be3f1c17a19150d95e5
Tolga <dfavenfre@protonmail.com>
2023-05-27 11:19:23 +0300

    Update econ_dashboard.py

--- econ_dashboard.py
+++ econ_dashboard.py
@@ -197,7 +197,13 @@ st.write(
 #model = from_pretrained_keras(model_url)
 
 
+import requests
+import pickle
 model_url = "https://huggingface.co/dfavenfre/model_use/resolve/main/model_use2.pkl"
+# Download the model file
+response = requests.get(model_url)
+with open("model_use2.pkl", "wb") as f:
+    f.write(response.content)
 # Load the model
 with open("model_use2.pkl", "rb") as f:
     model = pickle.load(f)
