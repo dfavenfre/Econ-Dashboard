880cf05dfb6e08507bde6bebd15d26059a910e22
Tolga <dfavenfre@protonmail.com>
2023-05-30 19:14:28 +0300

    Update scraping_scripts.py

--- scraping_scripts.py
+++ scraping_scripts.py
@@ -9,38 +9,49 @@ def fx_calendar():
     chrome_options = Options()
     chrome_options.add_argument("--window-size=1920,1080")
     chrome_options.add_argument("--headless")
-    driver = webdriver.Chrome(ChromeDriverManager().install(),options=chrome_options)
+    chrome_options.add_argument("--no-sandbox")  # Added for running in a virtual environment
+    chrome_options.add_argument("--disable-dev-shm-usage")  # Added for running in a virtual environment
+
+    # Set up virtual display using XVFB (X Virtual Frame Buffer)
+    xvfb_options = ["xvfb-run", "--auto-servernum", "--server-args='-screen 0 1920x1080x24'"]
+    driver = webdriver.Chrome(executable_path="/usr/bin/google-chrome", options=chrome_options, service_args=xvfb_options)
+    
     driver.get(url)
     
-    table_data = driver.find_element(By.XPATH,'//div[3][@class="table-responsive panel panel-default"]')
-    container = table_data.find_elements(By.XPATH,'//tr[@data-url]')
+    # Wait for the table to be loaded
+    table_data = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//div[3][@class="table-responsive panel panel-default"]')))
     
-    time=[]
-    currency=[]
-    event=[]
-    actual=[]
-    previous=[]
-    consensus=[]
-    forecast=[]
+    container = table_data.find_elements(By.XPATH, '//tr[@data-url]')
+    
+    time = []
+    currency = []
+    event = []
+    actual = []
+    previous = []
+    consensus = []
+    forecast = []
   
     for contain in container:
-        time.append(contain.find_element(By.XPATH,'./td[1]').text)
-        currency.append(contain.find_element(By.XPATH,'./td[2]').text)
-        event.append(contain.find_element(By.XPATH,'./td[3]').text)
-        actual.append(contain.find_element(By.XPATH,'./td[4]').text)
-        previous.append(contain.find_element(By.XPATH,'./td[5]').text)
-        consensus.append(contain.find_element(By.XPATH,'./td[6]').text)
-        forecast.append(contain.find_element(By.XPATH,'./td[7]').text)
+        time.append(contain.find_element(By.XPATH, './td[1]').text)
+        currency.append(contain.find_element(By.XPATH, './td[2]').text)
+        event.append(contain.find_element(By.XPATH, './td[3]').text)
+        actual.append(contain.find_element(By.XPATH, './td[4]').text)
+        previous.append(contain.find_element(By.XPATH, './td[5]').text)
+        consensus.append(contain.find_element(By.XPATH, './td[6]').text)
+        forecast.append(contain.find_element(By.XPATH, './td[7]').text)
 
     driver.quit()
 
-    data = pd.DataFrame({"time":time,
-                        "currency":currency,
-                        "event":event,
-                        "actual":actual,
-                        "consensus":consensus,
-                        "forecast":forecast,
-                        "previous":previous})
+    data = pd.DataFrame({
+        "time": time,
+        "currency": currency,
+        "event": event,
+        "actual": actual,
+        "consensus": consensus,
+        "forecast": forecast,
+        "previous": previous
+    })
+    
     return data
 
 def fetch_currencies():
